489675fcfacf9a422fe1b09cec56d9cbf7d651bb
Maxime Joly <maxime.m.joly@gmail.com>
2023-12-26 19:18:10 +0100

    feat(Parking): added parking service (#2)
    * chore(backend): quick corrections
    
    * feat(backend): add parking model and mutation.validation.ts
    
    * chore(backend): modify return to use responseStatus instead or res.status
    
    * feat(backend): finish base of parking service and push to history on deletion
    
    * chore(backend): clean

---
+++ main/service/services/parking/model/parking.model.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const evaluationSchema = new mongoose.Schema({
+    userId: String,
+    rating: { type: Number, min: 1, max: 5 },
+    comment: String,
+}, { _id: false });
+
+const parkingSchema = new mongoose.Schema({
+    ownerId: { type: String, required: true },
+    name: { type: String, required: true, trim: true, maxlength: 32 },
+    price: { type: Number, required: true },
+    geoLocation: {
+        type: { type: String, default: 'Point' },
+        coordinates: { type: [Number], required: true },
+    },
+    description: String,
+    evaluation: [evaluationSchema],
+    imagePaths: [String],
+    capacity: { type: Number, required: true },
+    active: { type: Boolean, default: true },
+}, { timestamps: true });
+
+const historyParkingSchema = new mongoose.Schema({
+    ownerId: { type: String, required: true },
+    name: { type: String, required: true, trim: true, maxlength: 32 },
+    price: { type: Number, required: true },
+    geoLocation: {
+        type: { type: String, default: 'Point' },
+        coordinates: { type: [Number], required: true },
+    },
+    description: String,
+    evaluation: [evaluationSchema],
+    imagePaths: [String],
+    capacity: { type: Number, required: true },
+    active: { type: Boolean, default: false },
+}, { timestamps: true });
+
+parkingSchema.index({ 'geoLocation.coordinates': '2dsphere' });
+
+export const History = mongoose.model('History', historyParkingSchema);
+
+export const Parking = mongoose.model('Parking', parkingSchema);
